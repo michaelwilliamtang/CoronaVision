53e5214b707fd6ae2d19e5866b8c3f4c0f360a06
Michael <michaelwilliamtang@gmail.com>
2020-05-31 18:06:20 -0700

    Added epi-related variables to map

--- Comb_Mapping.R
+++ Comb_Mapping.R
@@ -11,17 +11,16 @@ global <- read.csv(file.path(source_dir, "WHO-COVID-19-global-data.csv"), string
 global <- global %>% group_by(Country) %>%
   summarise(cumu_cases = max(Cumulative_cases),
             cumu_deaths = max(Cumulative_deaths))
-countries <- read.csv(file.path(source_dir, "countries.csv"), stringsAsFactors = F)
-clats <- countries$latitude
-clongs <- countries$longitude
-names(clats) <- countries$name
-names(clongs) <- countries$name
-global <- global %>% mutate(lats = clats[Country],
-                            longs = clongs[Country],
-                            radius = cumu_deaths * 0.002,
-                            lab = paste(global$Country, ": ", global$cumu_deaths, " deaths, ",
-                                        global$cumu_cases, " cases", sep = "")) %>%
-  na.omit() # sometimes non-states included for high nums, e.g. nyc
+countries <- read.csv(file.path(source_dir, "countries.csv"), stringsAsFactors = F) %>%
+  mutate(Country = name)
+global <- merge(global, countries, by = "Country") %>% 
+  na.omit() %>%
+  mutate(lats = latitude,
+         longs = longitude,
+         radius = cumu_deaths * 0.002,
+         lab = paste(Country, ": ", cumu_deaths, " deaths, ",
+                     cumu_cases, " cases", sep = ""))
+
 # plot country data
 # mp <- leaflet() %>% addTiles() %>% addHeatmap(data = global, lng = ~longs, lat = ~lats, intensity = ~cumu_deaths,
 #                                               blur = 20, max = 0.1, radius = 10, group = "COVID-19")
@@ -32,17 +31,16 @@ mp <- leaflet() %>% addTiles() %>%
 # source: https://www.cdc.gov/covid-data-tracker/index.html
 # read state data
 domestic <- read.csv(file.path(source_dir, "US_MAP_DATA.csv"), stringsAsFactors = F, skip = 2)
-states <- read.csv(file.path(source_dir, "states.csv"), stringsAsFactors = F)
-slats <- states$latitude
-slongs <- states$longitude
-names(slats) <- states$name
-names(slongs) <- states$name
-domestic <- domestic %>% mutate(lats = slats[jurisdiction],
-                                longs = slongs[jurisdiction],
-                                radius = Total.Death * 0.002,
-                                lab = paste(domestic$jurisdiction, ": ", domestic$Total.Death, " deaths, ",
-                                            domestic$Total.Cases, " cases", sep = "")) %>%
-  na.omit() # sometimes non-states included for high nums, e.g. nyc
+states <- read.csv(file.path(source_dir, "states.csv"), stringsAsFactors = F) %>%
+  mutate(jurisdiction = name)
+domestic <- merge(domestic, states, by = "jurisdiction") %>% 
+  na.omit() %>%
+  mutate(lats = latitude,
+         longs = longitude,
+         radius = Total.Death * 0.002,
+         lab = paste(jurisdiction, ": ", Total.Death, " deaths, ",
+                     Total.Cases, " cases", sep = ""))
+
 # plot state data
 # mp <- mp %>% addHeatmap(data = domestic, lng = ~longs, lat = ~lats, intensity = ~Total.Death,
 #                         blur = 20, max = 0.1, radius = 10, group = "COVID-19")
@@ -90,18 +88,14 @@ air_qual <- air_qual %>% mutate(longs = Longitude,
 #                                               blur = 20, max = 0.1, radius = 10)
 mp <- mp %>%
   addCircleMarkers(data = air_qual, lng = ~longs, lat = ~lats, radius = ~radius, stroke = F, color = "green",
-                   label = ~lab, group = "AQI")
-
-library(tidyverse)
-library(leaflet)
-library(leaflet.extras)
+                   label = ~lab, group = "Air Quality Index (AQI)")
 
 # source: https://data.giss.nasa.gov/gistemp/station_data_v4_globe/#form
 # read, merge temp data
-stations <- read_csv(file.path(source_dir, "v4.temperature.inv.csv")) %>%
+stations <- read.csv(file.path(source_dir, "v4.temperature.inv.csv"), stringsAsFactors = F) %>%
   mutate(Station = ID) %>%
   na.omit()
-samples <- read_csv(file.path(source_dir, "v4.mean_GISS_homogenized.csv")) %>%
+samples <- read.csv(file.path(source_dir, "v4.mean_GISS_homogenized.csv"), stringsAsFactors = F) %>%
   na.omit()
 comb_df <- merge(stations, samples, by = "Station")
 comb_df <- comb_df %>% mutate(longs = Lon,
@@ -116,9 +110,50 @@ mp <- mp %>%
   addCircleMarkers(data = comb_df, lng = ~longs, lat = ~lats, radius = ~radius, stroke = F, color = "#ff9966",
                    label = ~lab, group = "Temperature")
 
+# source: https://epi.envirocenter.yale.edu/epi-topline
+# read epi score data
+epi_df <- read.csv(file.path(source_dir, "epi2018scorescurrentv01.csv"), stringsAsFactors = F) %>%
+  na.omit() %>%
+  mutate(Country = country)
+epi_df$Country[which(epi_df$Country == "United States of America")] <- "United States"
+epi_df <- merge(epi_df, countries, by = "Country") %>%
+  na.omit() %>%
+  mutate(lats = latitude,
+         longs = longitude,
+         radius_epi = EPI.current * 0.3,
+         radius_bdh = BDH.current * 0.3,
+         radius_h2o = H2O.current * 0.3,
+         radius_ape = APE.current * 0.3,
+         radius_cce = CCE.current * 0.3,
+         lab_epi = paste(Country, ": EPI ", EPI.current, sep = ""),
+         lab_bdh = paste(Country, ": BDH ", BDH.current, sep = ""),
+         lab_h2o = paste(Country, ": H2O ", H2O.current, sep = ""),
+         lab_ape = paste(Country, ": APE ", APE.current, sep = ""),
+         lab_cce = paste(Country, ": CCE ", CCE.current, sep = ""))
+
+# plot epi: epi (environmental performance index), h20 (water quality), bdh (biodiversity), ape (air pollution),
+#   cce (energy emission)
+mp <- mp %>%
+  addCircleMarkers(data = epi_df, lng = ~longs, lat = ~lats, radius = ~radius_epi, stroke = F, color = "#99ff99",
+                   label = ~lab_epi, group = "Environmental Performance Index (EPI)")
+mp <- mp %>%
+  addCircleMarkers(data = epi_df, lng = ~longs, lat = ~lats, radius = ~radius_bdh, stroke = F, color = "#ff00ff",
+                   label = ~lab_bdh, group = "Biodiversity Score (BDH)")
+mp <- mp %>%
+  addCircleMarkers(data = epi_df, lng = ~longs, lat = ~lats, radius = ~radius_h2o, stroke = F, color = "#ccccff",
+                   label = ~lab_h2o, group = "Water Quality Score (H2O)")
+mp <- mp %>%
+  addCircleMarkers(data = epi_df, lng = ~longs, lat = ~lats, radius = ~radius_ape, stroke = F, color = "#ffff00",
+                 label = ~lab_ape, group = "Air Quality Score (APE)")
+mp <- mp %>%
+  addCircleMarkers(data = epi_df, lng = ~longs, lat = ~lats, radius = ~radius_cce, stroke = F, color = "#ff99ff",
+                   label = ~lab_cce, group = "Emissions Score (CCE)")
+
 # layer control
 mp <- mp %>% addLayersControl(
-  overlayGroups = c("COVID-19", "Water Contamination", "AQI", "Temperature"),
+  overlayGroups = c("COVID-19", "Water Contamination", "Air Quality Index (AQI)", "Temperature",
+                    "Environmental Performance Index (EPI)", "Biodiversity Score (BDH)", "Water Quality Score (H2O)",
+                    "Air Quality Score (APE)","Emissions Score (CCE)"),
   options = layersControlOptions(collapsed = FALSE)
 )
 mp
